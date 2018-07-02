0331c8a2e3b757610f9bf483b05411e6ca71fe00
rvaid <rvaid.29@gmail.com>
2018-07-03 01:26:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config ={
     "mode" : "filesystem", // mode : "aws" or mode : "filesystem"
     "aws" : {
              "Bucket_Name" : 's3dir740596',
-             "accessKeyId" : 'AKIAIDBA3EZC4B4KXREQ',
-             "secretAccessKey" : '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'
+             "accessKeyId" : '',
+             "secretAccessKey" : ''
     },
     "filesystem" : {
         "rootPath" : 'C:\\Users\\Rahul\\Desktop\\abc\\'
